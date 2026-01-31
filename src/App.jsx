5cce20c90ef468aeb30c478571b86a2fe56f7281
HikariNM <nicole.hikari.mascellino@gmail.com>
2026-01-31 10:42:01 +0100

    stilizzate le cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css'
 
 function App() {
@@ -20,17 +21,23 @@ function App() {
 
   return (
     <>
-      {actresses.map((person) => (
-        <div key={person.id}>
-          <img src={person.image} alt={person.name} />
-          <h3>{person.name}</h3>
-          <p>{person.birth_year}</p>
-          <p>{person.nationality}</p>
-          <p>{person.biography}</p>
-          <p>{person.awards}</p>
-        </div>
 
-      ))}
+      <div className='container my-4'>
+        <div className='row g-4 container justify-content-center'>
+          {actresses.map((person) => (
+
+            <div key={person.id} className="card bg-dark text-white col-12 col-md-3" >
+              <img src={person.image} className="card-img-top card-img-fixed" alt={person.name} />
+              <div className="card-body">
+                <h3 className="card-title">{person.name}</h3>
+                <p className="card-text fst-italic">{person.birth_year} - {person.nationality}</p>
+                <p className="card-text">{person.biography}</p>
+                <p className="card-text"><span className='fw-medium'>Awards:</span> {person.awards}</p>
+              </div>
+            </div>
+          ))}
+        </div >
+      </div >
     </>
   )
 }
