51bca88ed09614b03d450866fbcd2d0359d69662
HikariNM <nicole.hikari.mascellino@gmail.com>
2026-01-31 12:42:09 +0100

    inline + comments

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,28 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css'
 
 function App() {
+  // State che contiene la lista delle attrici
   const [actresses, setActresses] = useState([]);
+  // State che contiene la lista degli attori
   const [actors, setActors] = useState([]);
+  // State unico per attori e attrici
   const [mergedList, setMergedList] = useState([]);
 
   function getData() {
+    // URL base dell'API
     const apiUrl = 'https://lanciweb.github.io/demo/api/'
     const apiUrlActresses = 'actresses/'
     const apiUrlActors = 'actors/'
 
+    // Chiamata API per le attrici
     axios.get(`${apiUrl}${apiUrlActresses}`).then(response => {
       console.log('Questi sono i dati delle attrici:', response.data);
       setActresses(response.data);
     }).catch(error => {
       console.log('Si è rotto...', error);
     })
+
+    // Chiamata API per gli attori
     axios.get(`${apiUrl}${apiUrlActors}`).then(response => {
       console.log('Questi sono i dati degli attori:', response.data);
       setActors(response.data);
@@ -27,10 +34,12 @@ function App() {
     })
   };
 
+  // Unisce attori e attrici in un unico array 
   function mergeList() {
     setMergedList([...actresses, ...actors])
   };
 
+  // Controlla se l'oggetto passato ha uno dei due campi e restituisce i film
   function getMovies(obj) {
     if (obj.most_famous_movies) {
       return obj.most_famous_movies.map((movie, i) => <li key={i}>{movie}</li>)
@@ -40,6 +49,10 @@ function App() {
     }
   };
 
+
+  // Controlla se il campo awards è un array o una stringa 
+  // se è un array unisce con ', '
+  // se è una stringa lo restituisce così com'è
   function getAwards(awards) {
     if (Array.isArray(awards)) {
       return awards.join(', ')
@@ -48,8 +61,12 @@ function App() {
     }
   };
 
+
+  // Cosa fare, [quando farlo]
+  // getData viene eseguito all'avvio della pagina
   useEffect(getData, []);
 
+  // mergeList viene eseguito nel momento in cui i parametri in actresses e/o actors cambiano
   useEffect(mergeList, [actresses, actors]);
 
 
